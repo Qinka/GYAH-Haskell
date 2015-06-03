9ed9410dc85b49a6b30ea8204667b0e0f9241ec1
Qinka <qinka@live.com>
2015-06-03 19:09:12 +0800

    ??

--- Setup.hs
+++ Setup.hs
@@ -1,21 +1,32 @@
 import Distribution.Simple
 import System.IO
 import System.Directory
+import System.Environment
+import Data.List
 main = do
     --your
-    dir <- getAppUserDataDirectory "GiveYouAHead"
-    isE <- doesDirectoryExist dir
-    if isE == True then putStrLn "" else createDirectory dir
-    isE <- doesDirectoryExist (dir++"/data")
-    if isE == True then putStrLn "" else createDirectory (dir++"/data")
-    isE <- doesDirectoryExist (dir++"/data/language")
-    if isE == True then putStrLn "" else createDirectory (dir++"/data/language")
-    writeFile (dir ++ "/data/language/Haskell.cmap") (show langM)
+    (x:_) <- getArgs
+    if x == "configure" then do
+            dir <- getAppUserDataDirectory "GiveYouAHead"
+            isE <- doesDirectoryExist dir
+            if isE == True then putStrLn "" else createDirectory dir
+            isE <- doesDirectoryExist (dir++"/data")
+            if isE == True then putStrLn "" else createDirectory (dir++"/data")
+            isE <- doesDirectoryExist (dir++"/data/language")
+            if isE == True then putStrLn "" else createDirectory (dir++"/data/language")
+            hD <- openFile (dir++"/data/delList.dat") ReadMode
+            stSrc <- hGetLine hD
+            hClose hD
+            putStrLn stSrc
+            writeFile (dir ++ "/data/delList.dat") (show$dropRepeated$sort((read stSrc ::[String])++dL))
+            writeFile (dir ++ "/data/language/Haskell.cmap") (show langM)
+            defaultMain
     --your end
-    defaultMain
+        else
+            defaultMain
     where
     langM = [
-        ("*Template","main = return ()\n"),
+        ("*Template","\nmain = return ()\n"),
         ("*NoteMark","--"),
         ("*SrcAhead","Haskell_"),
         ("*SrcBack",".hs"),
@@ -23,5 +34,21 @@ main = do
         ("*COE",""),
         ("*ImportAhead","import "),
         ("*ImportBack","\n"), -- need break a new line
-        ("*FE",".hs"),
+        ("*FE","hs"),
+        ("*Compiler","ghc"),
+        ("*Debug"," "),
+        ("*Object","-o")
         ]
+    dL = [
+        "*.hs~",
+        "*.hi",
+        "*.o"
+        ]
+
+
+dropRepeated :: (Eq a)=> [a] -> [a]
+dropRepeated [] = []
+dropRepeated (x:[]) = [x]
+dropRepeated (x:y:xs)
+    | x == y = dropRepeated (x:xs)
+    | otherwise = x:dropRepeated (y:xs)
